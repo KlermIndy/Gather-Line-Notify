317f867e6e2b5092bc7344896b75855a522ed142
Klerm Indy <klermindy@gmail.com>
2023-12-08 21:45:32 +0700

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Gather-Line-Notify
+# Gather mod example: basic player listener
+
+basic example of subscribing to player movement in a space
+
+check out the [websocket API docs](https://gathertown.notion.site/Gather-Websocket-API-bf2d5d4526db412590c3579c36141063) for more!
+
+## setup
+
+prereq: have NodeJS and npm installed
+
+run `npm install`
+
+put your API key in a file named `api-key.js` like so:
+
+```js
+exports.API_KEY = "your-api-key-here";
+```
+
+## running
+
+`node index.js`
+
+URL
+- https://gathertown.notion.site/Gather-Websocket-API-bf2d5d4526db412590c3579c36141063
+- http://gather-game-client-docs.s3-website-us-west-2.amazonaws.com/classes/Game.html
---
+++ index.js
@@ -0,0 +1,75 @@
+const axios = require('axios');
+const qs = require('qs');
+
+const { Game } = require("@gathertown/gather-game-client");
+global.WebSocket = require("isomorphic-ws");
+
+/**** setup ****/
+
+// Gather
+const API_KEY = "gBy23gweuIrpIInM";
+const SPACE_ID = "xBP58fLWr9drozxs\\ThaiBev_IT";
+// Line Notify
+const LINE_NOTIFY_TOKEN = "Bearer mCB4ti6jBFRqcyU6bso7ntk2jRPVWBbig66q7MXSGcU";
+
+// what's going on here is better explained in the docs:
+// https://gathertown.notion.site/Gather-Websocket-API-bf2d5d4526db412590c3579c36141063
+const game = new Game(SPACE_ID, () => Promise.resolve({ apiKey: API_KEY }));
+// replace with your spaceId of choice ^^^^^^^^^^^
+game.connect();
+game.subscribeToConnection((connected) => console.log("connected?", connected));
+
+/**** the good stuff ****/
+
+// game.subscribeToEvent("playerMoves", (data, context) => {
+//   console.log(
+//     context?.player?.name ?? context.playerId,
+//     "moved in direction",
+//     data.playerMoves.direction,
+//     // "in map id",
+//     // data.playerMoves.mapId
+//   );
+//   console.log(data);
+// });
+
+game.subscribeToEvent("playerChats", (data, context) => {
+  console.log(
+    context?.player?.name ?? context.playerId,
+    "message to you :",
+    data.playerChats.contents
+  );
+  // console.log(data);
+  // console.log(context);
+  // console.log(context.target);
+  
+
+  // data.playerChats.senderId me : y06Wp4JXwOUCUIhasFA5f0vYikx2
+
+
+  const playerName = context?.player?.name ?? context.playerId
+  const dataQs = qs.stringify({
+    'message': `${playerName} message to you : ${data.playerChats.contents}`
+  });
+
+  const config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'https://notify-api.line.me/api/notify',
+    headers: { 
+      'Content-Type': 'application/x-www-form-urlencoded', 
+      'Authorization': LINE_NOTIFY_TOKEN
+    },
+    data : dataQs
+  };
+
+  axios.request(config)
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+
+
+});
